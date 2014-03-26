f948cf7849f4876dcbfaf0657b025c74f783ad3a
ddykstra <doug.dykstra@sparcedge.com>
2014-03-26 17:44:15 -0400

    First pass at the Give a Sparcet automation

---
+++ teamphoria_givesparcet.rb
@@ -0,0 +1,21 @@
+/class name			:class_name			:class
+css selector		:css	 
+id					:id	 
+link text			:link_text			:link
+name				:name	 
+partial link text	:partial_link_text	 
+tag name			:tag_name	 
+xpath				:xpath/
+
+require "selenium-webdriver"
+browser = Selenium::WebDriver.for :firefox
+browser.get "http://ui-dev.teamphoria.com/login"
+browser.current_url
+#browser.find_element(link_text: "Images").click
+browser.title
+browser.find_element(name: "email").send_keys "doug.dykstra@sparcedge.com"
+browser.find_element(name: "password").send_keys "password"
+browser.find_element(css: "#login-container > form > fieldset > button").click
+browser.find_element(css: "#login-container > div > ul > li:nth-child(6) > a > h3").click
+#browser.find_elements(tag_name: "img").size
+browser.close
