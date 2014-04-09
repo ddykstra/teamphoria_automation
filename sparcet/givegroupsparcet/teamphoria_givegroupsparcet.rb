4cd94ffff868a1e03a31c46a12b65d95318b8e5e
ddykstra <doug.dykstra@sparcedge.com>
2014-04-09 15:20:58 -0400

    added give a group sparcet automation

---
+++ sparcet/givegroupsparcet/teamphoria_givegroupsparcet.rb
@@ -0,0 +1,71 @@
+/class name			:class_name			:class
+css selector		:css	 
+id					:id	 
+link text			:link_text			:link
+name				:name	 
+partial link text	:partial_link_text	 
+tag name			:tag_name	 
+xpath				:xpath/
+
+#Give a group Sparcet automation
+
+require "selenium-webdriver"
+ browser = Selenium::WebDriver.for :firefox
+  browser.get "http://teamphoria-web-dev.elasticbeanstalk.com/"
+  #browser.window.resize_to(1680,1050)
+   browser.current_url
+	browser.title
+
+	#Login screen
+			browser.find_element(name: "email").send_keys "sparcet4@gmail.com"
+			browser.find_element(name: "password").send_keys "P@55word1"
+			browser.find_element(css: "#login-container > form > fieldset > button").click
+
+	#This for tenant selection post login
+		#browser.find_element(css: "#login-container > div > ul > li:nth-child(7) > a > h3").click 
+
+	#Give Sparcet button
+			browser.find_element(css: "#vm-options > a.nav-link.nav-link-primary > span").click
+			sleep(15)
+
+
+	#Sparcet details
+			browser.find_element(id: "give-input-selectize").click
+			browser.find_element(id: "give-input-selectize").send_keys "josh.garwood"
+			sleep(5)
+			browser.find_element(id: "give-input-selectize").send_keys :tab
+			browser.find_element(id: "give-input-selectize").send_keys "prashant.katwa"
+			sleep(5)
+			browser.find_element(id: "give-input-selectize").send_keys :tab
+
+			browser.find_element(name: "impactId").click
+			sleep(2)
+			browser.find_element(css: "#fullscreen-views > div > div > form > fieldset.send-message > ol > li:nth-child(1) > select > option:nth-child(2)").click
+			 browser.find_element(css: "#fullscreen-views > div > div > form > fieldset.send-message > ol > li:nth-child(1) > select > option:nth-child(2)").send_keys :arrow_down
+			  browser.find_element(css: "#fullscreen-views > div > div > form > fieldset.send-message > ol > li:nth-child(1) > select > option:nth-child(2)").send_keys :arrow_down
+			   browser.find_element(css: "#fullscreen-views > div > div > form > fieldset.send-message > ol > li:nth-child(1) > select > option:nth-child(2)").send_keys :tab
+
+			browser.find_element(name: "coreValueId").click
+			sleep(2)
+			browser.find_element(css: "#fullscreen-views > div > div > form > fieldset.send-message > ol > li:nth-child(2) > select > option:nth-child(4)").click
+			 browser.find_element(css: "#fullscreen-views > div > div > form > fieldset.send-message > ol > li:nth-child(2) > select > option:nth-child(4)").send_keys :arrow_down
+			  browser.find_element(css: "#fullscreen-views > div > div > form > fieldset.send-message > ol > li:nth-child(2) > select > option:nth-child(4)").send_keys :arrow_down
+			   browser.find_element(css: "#fullscreen-views > div > div > form > fieldset.send-message > ol > li:nth-child(2) > select > option:nth-child(4)").send_keys :arrow_down
+			    browser.find_element(css: "#fullscreen-views > div > div > form > fieldset.send-message > ol > li:nth-child(2) > select > option:nth-child(4)").send_keys :arrow_down
+			     browser.find_element(css: "#fullscreen-views > div > div > form > fieldset.send-message > ol > li:nth-child(2) > select > option:nth-child(4)").send_keys :tab
+
+		#Badge select
+			browser.find_element(xpath: "//*[@id='fullscreen-views']/div/div/form/fieldset[1]/div[1]").click 
+
+		#Sparcet Reason and Thanks message; send click
+			browser.find_element(id: "reason").send_keys "This Group Sparcet was sent through automation"
+			 browser.find_element(name: "thanksMessage").send_keys "Automated Group Sparcet"
+			  browser.find_element(class_name: "action-submit").click
+
+	#wait.until(ExpectedConditions.elementToBeClickable(By.class "selectize-control"));
+	#/WebDriverWait wait = new WebDriverWait(webDriver, timeoutInSeconds);
+	#wait.until(ExpectedConditions.visibilityOfElementLocated(By.class "selectize-control"));/
+	#browser.find_element(link_text: "Images").click
+	#browser.find_elements(tag_name: "img").size
+
+	browser.close
